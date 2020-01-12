0656a103f015337791ced7cd59d251695c640152
Matiusz <matszwecja@gmail.com>
2020-01-12 15:00:48 +0100

    working mp3_player import

---
+++ Debug/Src/mp3dec.d
@@ -0,0 +1,10 @@
+Src/mp3dec.o: ../Src/mp3dec.c ../Inc/pub/mp3common.h ../Inc/mp3dec.h \
+ ../Inc/platform.h ../Inc/pub/statname.h
+
+../Inc/pub/mp3common.h:
+
+../Inc/mp3dec.h:
+
+../Inc/platform.h:
+
+../Inc/pub/statname.h:
---
+++ Debug/Src/mp3tabs.d
@@ -0,0 +1,10 @@
+Src/mp3tabs.o: ../Src/mp3tabs.c ../Inc/pub/mp3common.h ../Inc/mp3dec.h \
+ ../Inc/platform.h ../Inc/pub/statname.h
+
+../Inc/pub/mp3common.h:
+
+../Inc/mp3dec.h:
+
+../Inc/platform.h:
+
+../Inc/pub/statname.h:
